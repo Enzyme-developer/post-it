38b788415532dd34ae9318c179aeac78a2473b02
Ayoe <dannyhopkins20003@gmail.com>
2023-06-15 07:58:28 +0100

    first commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from '../../../prisma/client'
+import type { Adapter } from "next-auth/adapters"
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+  ],
+});
